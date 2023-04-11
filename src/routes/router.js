3625e944700dc8a22a9f3af05abf9f040a928317
JaViKo <javikogutierrez64@gmail.com>
2023-04-10 22:45:04 -0500

    config lazy load

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,15 +1,22 @@
-import AboutPage from '@/modules/pokemon/pages/AboutPage'
-import ListPage from '@/modules/pokemon/pages/ListPage'
-import PokemonPage from '@/modules/pokemon/pages/PokemonPage'
-import NotPageFound from '@/modules/shared/pages/NotPageFound'
-
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-    { path: '/', component: ListPage },
-    { path: '/about', component: AboutPage },
-    { path: '/id', component: PokemonPage },
-    { path: '/:pathMatch(.*)*', component: NotPageFound }
+    { 
+        path: '/', 
+        component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage') 
+    },
+    { 
+        path: '/about', 
+        component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage')
+    },
+    { 
+        path: '/id', 
+        component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage') 
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        component: () => import(/* webpackChunkName: "NotPageFound" */ '@/modules/shared/pages/NotPageFound')
+    }
 ]
 
 const router = createRouter({
