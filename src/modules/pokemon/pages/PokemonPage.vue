e19f05877453689aac4a91fd636a7b3a9d35588c
JaViKo <javikogutierrez64@gmail.com>
2023-04-11 22:26:07 -0500

    http request

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>PokemonPage: <span>#{{ id }}</span></h1>
+    <div class="" v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :altAlt="pokemon.name">
+    </div>
 </template>
 
 <script>
@@ -13,6 +16,7 @@ export default {
     data(){
         return {
             // id: null
+            pokemon: null
         }
     },
     created() {
@@ -20,6 +24,24 @@ export default {
         // const {id} = this.$route.params
         // this.id = id
         // console.log(id);
+        this.getPokemon()
     },
+    methods: {
+        async getPokemon() {
+            try {
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then( r => r.json() )
+                console.log(pokemon);
+                this.pokemon = pokemon;
+            } catch (error) {
+                console.log(this.$router);
+                this.$router.push('/');
+            }
+        }
+    },
+    watch: {
+        id() {
+            this.getPokemon()
+        }
+    }
 }
 </script>
