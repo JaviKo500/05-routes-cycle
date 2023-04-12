0a9c5299df87f9d41bf3951517d197faab0f586e
JaViKo <javikogutierrez64@gmail.com>
2023-04-11 21:03:38 -0500

    life cycle

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,63 @@
 <template>
     <h1>About Page</h1>
+    <hr>
+    <h2>{{ name }}</h2>
+    <button @click="onChangeName">
+        Change Name
+    </button>
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            name: 'Javiko'
+        }
+    },
+    methods: {
+        onChangeName (){
+            console.log('Change name');
+            this.name = 'Jonnathan'
+        }
+    },  
+    beforeCreate(){
+        console.log('beforeCreate');
+    },
+    created(){
+        console.log('created');
+    },
+    beforeMount(){
+        console.log('beforeMount');
+    },
+    mounted(){
+        console.log('mounted');
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate');
+    },
+    updated(){
+        console.log('updated');
+    },
+    activated(){
+        console.log('activated');
+    },
+    deactivated(){
+        console.log('deactivated');
+    },
+    beforeUnmount(){
+        console.log('beforeUnmount');
+    },
+    unmounted(){
+        console.log('unmounted');
+    },
+    errorCaptured(){
+        console.log('errorCaptured');
+    },
+    renderTracked(){
+        console.log('renderTracked');
+    },
+    renderTriggered(){
+        console.log('renderTriggered');
+    },
+}
+</script>
