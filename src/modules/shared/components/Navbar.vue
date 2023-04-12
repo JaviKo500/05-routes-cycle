52fc89122b121d46d2848868773159aaf8bae4b8
JaViKo <javikogutierrez64@gmail.com>
2023-04-11 23:02:44 -0500

    router link

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,30 @@
 <template>
     <div>
-        <router-link :to ="{name: 'home'}">Pokemon List</router-link>
+        <!-- <router-link :to ="{name: 'home'}">Pokemon List</router-link>
         <router-link :to ="{name: 'pokemon-id', params: { id: 100 } }">Pokemon id</router-link>
-        <router-link :to ="{name: 'about'}">About</router-link>
+        <router-link :to ="{name: 'about'}">About</router-link> -->
+
+        <CustomLink v-for="link in links" :key="link.to" :link="link" />
     </div>
 </template>
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    data() {
+        return {
+            links: [
+                { to: '/', name: 'Pokemons'},
+                { to: '/50', name: 'By id'},
+                { to: '/about', name: 'About'},
+                { to: 'https://google.com', name: 'Google'},
+            ]
+        }
+    },  
+    components: {
+        CustomLink: defineAsyncComponent( () => import('./CustomLink') )
+    }
+}
+</script>
 <style scoped>
     div {
         padding: 0, 30px;
@@ -16,8 +36,8 @@
         margin: 0 10px;
     }
 
-    div .router-link-exact-active {
+    /* div .router-link-exact-active {
         color: #42b983;
-    }
+    } */
 
 </style>
